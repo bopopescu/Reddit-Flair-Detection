32bbde130bec0c4ee0a7e249fb3a8f589d29404d
Anshuman Pati <anshpati23@gmail.com>
2020-04-27 04:16:17 +0530

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,81 +1,99 @@
-import scripts.textcleaning as tc
-from scripts.plot import create_plot
-import pickle
-import logging
-import gensim
-import praw
-from praw.models import MoreComments
-from zipfile import ZipFile 
+import os
+from flask import jsonify, Flask, render_template, request, redirect, url_for, send_from_directory
+import six.moves.urllib as urllib
+import numpy as np
+import requests
+import urllib.request, json 
+import time
+# from pytorch_pretrained_bert.tokenization import BertTokenizer
+# from flask_cors import CORS
+import datetime
+import torch
+# from pytorch_pretrained_bert.modeling import BertForSequenceClassification
+from helper import bert_predict
 
-# file_name = "model/random_forest_model.bin.zip"
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# Use pickle to load in the pre-trained model
-model = pickle.load(open('model/LR.pkl','rb'))
+label_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+max_seq_length = 256
+id_to_label = {0: 'AskIndia',
+ 1: 'Business/Finance',
+ 2: 'Food',
+ 3: 'Non-Political',
+ 4: 'Photography',
+ 5: 'Policy/Economy',
+ 6: 'Politics',
+ 7: 'Scheduled',
+ 8: 'Science/Technology',
+ 9: 'Sports',
+ 10: '[R]eddiquette'}
 
+bert_model = 'bert-base-uncased'
+model_file = 'pytorch_model.bin'
 
-reddit = praw.Reddit(client_id = "JTqwrVU0MISDWg",
-					client_secret = "xe06s55FgF5JDSHABI2AsnrX1Q0",
-					user_agent = "reddit_scraper",
-					username = "divyanshuggrwl",
-					password = "wateringplants")
+tokenizer = BertTokenizer.from_pretrained(bert_model, do_lower_case=True)
+model_state_dict = torch.load(model_file, map_location='cpu')
+model = BertForSequenceClassification.from_pretrained(bert_model, state_dict=model_state_dict, num_labels=11)
 
-def prediction(url):
-	submission = reddit.submission(url = url)
-	data = {}
-	data["title"] = str(submission.title)
-	data["url"] = str(submission.url)
-	data["body"] = str(submission.selftext)
+@app.route('/', methods=['GET'])
+def root():
+    return render_template('index.html')
 
-	submission.comments.replace_more(limit=None)
-	comment = ''
-	count = 0
-	for top_level_comment in submission.comments:
-		comment = comment + ' ' + top_level_comment.body
-		count+=1
-		if(count > 10):
-		 	break
-		
-	data["comment"] = str(comment)
+@app.route('/render', methods=['POST'])
+def render():
+    check = ['https']
+    url = request.data.decode('utf-8')
+    if url:
+        try:
+            r = requests.get(url + '.json', headers = {'User-agent': 'your bot 0.1'})
+            data = r.json()
+            comments = list()
 
-	data['title'] = tc.clean_text(str(data['title']))
-	data['body'] = tc.clean_text(str(data['body']))
-	data['comment'] = tc.clean_text(str(data['comment']))
-    
-	combined_features = data["title"] + data["comment"] + data["body"] + data["url"]
+            x = data[0]['data']['children'][0]['data']
+            y = data[1]['data']['children']
 
-	return model.predict([combined_features])
+            author = x['author']
+            date = x['created']
+            title = x['title']
+            selftext = x['selftext']
+            flair = x["link_flair_text"]
+            score = x["score"]
+            num_comments = x["num_comments"]
+            image_src = x['url']
+            print (image_src)
+            if ".jpg" or ".png" in image_src:
+            	image_src = image_src.split('/')[-1].split('.')
+       	    
+       	    print (image_src)
+       	    date = datetime.datetime.fromtimestamp(date).date().strftime("%Y-%m-%d %H:%M")
+            for comment in y:
+                c = comment['data']['body']
+                comments.append(c)
 
+            label = bert_predict(model, title, selftext, tokenizer, id_to_label, label_list, max_seq_length)
 
-import flask
-import pickle
-import pandas as pd
+            features = {
+            'title': title,
+            'date': date,
+            'author': author,
+            'selftext': selftext,
+            'flair': flair,
+            'score': score,
+            'num_comments': num_comments,
+            'image_src': image_src,
+            'comments': comments,
+            'label': label
+            }
 
+            return jsonify(features)
+        except:
+            return jsonify('Invalid Input')
 
-# Initialise the Flask app
-app = flask.Flask(__name__, template_folder='templates')
-
-# Set up the main route
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    if flask.request.method == 'GET':
-        # Just render the initial form, to get input
-        return(flask.render_template('main.html'))
-    
-    if flask.request.method == 'POST':
-        # Extract the input
-        text = flask.request.form['url']
-        
-        # Get the model's prediction
-        flair = prediction(str(text))
-    
-        # Render the form again, but add in the prediction and remind user
-        # of the values they input before
-        return flask.render_template('main.html', original_input={'url':str(text)}, result=flair,)
-@app.route("/stats")
-def stats():
-	bar = create_plot()
-	return flask.render_template('graph.html',plot=bar)
+    else:
+        return jsonify('Invalid Input')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=80)
